327dd9349a7301b3ce7998c4a7518266658f6103
piaand <pia.m.andersin@gmail.com>
2020-05-12 14:02:51 +0300

    added onduty status update functionality

--- application/hogs/views.py
+++ application/hogs/views.py
@@ -1,5 +1,5 @@
 from application import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from application.hogs.models import Hog
 
 @app.route("/hogs", methods=["GET"])
@@ -10,10 +10,19 @@ def hogs_index():
 def hogs_form():
     return render_template("hogs/new.html")
 
+@app.route("/hogs/<hog_id>/", methods=["POST"])
+def hog_set_onduty(hog_id):
+
+    hog = Hog.query.get(hog_id)
+    hog.onduty = True
+    db.session().commit()
+  
+    return redirect(url_for("hogs_index"))
+
 @app.route("/hogs/", methods=["POST"])
 def hogs_create():
     hog = Hog(request.form.get("name"))
 
     db.session().add(hog)
     db.session().commit()
-    return "hello world!"
+    return redirect(url_for("hogs_index"))
