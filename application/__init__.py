dbe8e8d1a9384df65612a9cf293a9470df644a36
piaand <pia.m.andersin@gmail.com>
2020-05-12 13:40:22 +0300

    added first get/post request functions

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
+
+# Use db hogs -- /// means hogs.db is at the same place as rest of the app
+# Echo True means print out all queries
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hogs.db"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
 from application import views
+
+from application.hogs import models
+from application.hogs import views
+
+db.create_all()
