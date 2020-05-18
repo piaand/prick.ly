cb08e69b6ab13774ea6754d5d4cac5ac7232bf40
piaand <pia.m.andersin@gmail.com>
2020-05-18 16:44:42 +0300

    added simple signup function

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
 from application.auth.forms import LoginForm
+from application.auth.forms import SignupForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -24,3 +25,25 @@ def auth_login():
 def auth_logout():
     logout_user()
     return redirect(url_for("index"))  
+
+@app.route("/auth/signup", methods = ["GET", "POST"])
+def auth_signup():
+    if request.method == "GET":
+        return render_template("auth/signupform.html", form = SignupForm())
+
+    form = SignupForm(request.form)
+    
+    if not form.validate():
+        return render_template("auth/signupform.html", form = form)
+
+    user = User.query.filter_by(username=form.username.data).first()
+    if user:
+        return render_template("auth/signupform.html", form = form,
+                               error = "This username is already taken")
+        
+    account = User(form.name.data, form.username.data, form.password.data)
+
+    db.session().add(account)
+    db.session().commit()
+    
+    return redirect(url_for("index"))
