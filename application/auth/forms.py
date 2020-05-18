3e1d4b3e7605fae46202c047a3ae06a91c852154
piaand <pia.m.andersin@gmail.com>
2020-05-18 16:44:42 +0300

    added simple signup function

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
   
+    class Meta:
+        csrf = False
+
+class SignupForm(FlaskForm):
+    name = StringField("Your full name", [validators.Length(min=3), validators.length(max=50)])
+    username = StringField("Your username", [validators.Length(min=3), validators.length(max=50)])
+    password = PasswordField("Password", [validators.Length(min=3), validators.length(max=50)])
+    
     class Meta:
         csrf = False
