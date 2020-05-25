036de63c4e76310972fc5daf7a5ff844c46fbdc1
piaand <pia.m.andersin@gmail.com>
2020-05-25 17:41:15 +0300

    added a hedgehog association table adn view

--- application/reservations/forms.py
+++ application/reservations/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import NumberRange
 
 class ReservationForm(FlaskForm):
     duration = IntegerField("How long session (in minutes)", validators=[NumberRange(min=5, max=60, message='one session should be between 5 and 60 minutes')])
+    hog = StringField("Name of the hedgehog", validators=[validators.Length(min=3)])
 
     class Meta:
         csrf = False
