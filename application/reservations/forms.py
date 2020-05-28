61e7f40bf9970c196c251e89fa9cbc93baa6dda0
piaand <pia.m.andersin@gmail.com>
2020-05-28 16:24:21 +0300

    user is able to delete the reservation

--- application/reservations/forms.py
+++ application/reservations/forms.py
@@ -4,13 +4,13 @@ from wtforms.validators import NumberRange
 
 class ReservationForm(FlaskForm):
     duration = IntegerField("How long session (in minutes)", validators=[NumberRange(min=5, max=60, message='one session should be between 5 and 60 minutes')])
-    hog = StringField("Name of the hedgehog", validators=[validators.Length(min=3)])
+    hog = StringField("Name of the hedgehog:", validators=[validators.Length(min=3)])
 
     class Meta:
         csrf = False
         
 class SummaryForm(FlaskForm):
-    hog = StringField("Name of the hedgehog", validators=[validators.Length(min=3)])
+    hog = StringField("Write the name of the hedgehog:", validators=[validators.Length(min=3)])
 
     class Meta:
         csrf = False
